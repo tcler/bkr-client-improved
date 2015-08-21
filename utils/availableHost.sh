4e096adef5230b8ef6eb12ef27ddcd03925ee47d
stevens.yin <jiyin@redhat.com>
2015-08-21 17:00:34 +0800

    Update arch list in availableHost.sh

--- utils/availableHost.sh
+++ utils/availableHost.sh
@@ -2,7 +2,7 @@
 # author jiyin@redhat.com
 
 available_host() {
-	for arch in x86_64 ppc64 s390x i386 ppc64le armhfp; do
+	for arch in x86_64 ppc64 s390x i386 ppc64le aarch64 armhfp; do
 		echo "${arch}_host $(bkr list-systems --available --arch=$arch | wc -l)"
 	done
 }
