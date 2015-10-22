d321aace3d791ecbdf2abaefb5c68c71ae63d9b6
stevens.yin <jiyin@redhat.com>
2015-10-22 20:28:04 +0800

    1. Optimize host count code, avoid count error. 2. Add recipe chk at head of monitor, exit if crossed the cordon.

--- utils/recipe.sh
+++ utils/recipe.sh
@@ -9,24 +9,29 @@ HostMax[i386]=65
 HostMax[ppc64]=24
 HostMax[s390x]=10
 HostMax[ppc64le]=10
+ret=0
+users="$1"
 
 recipeCheck() {
 	local arch=${1:-x86_64}
 	local user=${2:-jiyin}
-	I=$(curl http://nest.test.redhat.com/mnt/qa/scratch/azelinka/beaker-stats/top-recipe-owners.$arch 2>/dev/null)
+	local url=http://nest.test.redhat.com/mnt/qa/scratch/azelinka/beaker-stats/top-recipe-owners.$arch
+	I=$(echo $url; curl $url 2>/dev/null)
 	M=$(echo "$I" | awk '$1 ~ "'$user'" {print $1, $2}')
 	while read U N; do
 		[ "${N:-0}" -gt ${HostMax[$arch]} ] && {
-			echo "$I" | sendmail.sh -p '[Remind warning line kkkkkkkk] ' -t "$U@redhat.com" -c "jiyin@redhat.com" - \
+			let ret++
+			echo "$I" | sendmail.sh -p '[Remind warning line kkkkk] ' -t "$U@redhat.com" -c "jiyin@redhat.com" - \
 				"You($U) hold too much($N/${HostMax[$arch]}) $arch recipes"  &>/dev/null
 		}
 	done <<<"$M"
 	echo -e "\033[1;34m$arch host usage status\033[0m:\n$I"
 }
 
-users="^(jiyin|userA|userB)$"
+users="${users:-^(jiyin)$}"
 recipeCheck x86_64  "$users"
 recipeCheck ppc64   "$users"
 recipeCheck s390x   "$users"
 recipeCheck i386    "$users"
 
+exit $ret
