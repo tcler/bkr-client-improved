f2e0393c24175588797f5a8361eecc07d065cf75
tony.yin <jiyin@redhat.com>
2015-12-23 15:08:32 +0800

    Improve wub server for multi-user mode

--- utils/wub-service.sh
+++ utils/wub-service.sh
@@ -1,33 +1,58 @@
-#! /bin/sh
+#! /bin/bash
 #
 #	Start/Stop Wub server
 
 wubdir=/opt/wub
+port=$(($(id -u) + 7080))
+confdir=~/.wub
 pushd $wubdir >/dev/null
+
+mkdir -p $confdir
+sed "/-port .*HTTP listener/s/8080/$port/" site.config >$confdir/site.config
+
+gethostname() {
+	local host name domain hash coment
+
+	HOST_FILE=/etc/redhat-ddns/hosts
+	if [ -f "$HOST_FILE" ]; then
+		read name domain hash comment < <(sed '/^\s*$/d; /^\s*#/d' $HOST_FILE)
+		host=${name}.${domain}
+	else
+		read host _ <<< "$(hostname -A)"
+	fi
+	echo $host
+}
+wubstat() {
+	ps -U $USER -u $USER -o pid,user:16,cmd|grep -v grep|grep tclsh.*Wub/Application.tcl &&
+		echo -e "$(gethostname):$port"
+}
 
 case "$1" in
   start)
 	echo "Starting Wub server"
-	ps axf|grep -v grep|grep tclsh.*Wub/Application.tcl && exit 0
+	ps -U $USER -u $USER -o pid,user:20,cmd|grep -v grep|grep tclsh.*Wub/Application.tcl && exit 0
 	nohup tclsh8.6 Wub/Application.tcl 2>/dev/null &
 	rm -f nohup.out
-	ps axf|grep -v grep|grep tclsh.*Wub/Application.tcl
+	wubstat
 	;;
   stop)
 	echo "Stoping Wub server"
-	kill $(ps axf|grep -v grep|grep 'tclsh8.6.*Wub/Application.tcl'|awk '{print $1}')
+	kill $(ps -U $USER -u $USER -o pid,user:20,cmd|grep -v grep|grep 'tclsh8.6.*Wub/Application.tcl'|awk '{print $1}')
 	sleep 1
 	;;
   restart)
 	echo "Retarting Wub server"
-	kill $(ps axf|grep -v grep|grep 'tclsh8.6.*Wub/Application.tcl'|awk '{print $1}')
+	kill $(ps -U $USER -u $USER -o pid,user:20,cmd|grep -v grep|grep 'tclsh8.6.*Wub/Application.tcl'|awk '{print $1}')
 	sleep 1
 	nohup tclsh8.6 Wub/Application.tcl 2>/dev/null &
 	rm -f nohup.out
-	ps axf|grep -v grep|grep tclsh.*Wub/Application.tcl
+	wubstat
+	;;
+  stat|status)
+	wubstat
 	;;
   *)
-	echo "Usage: $0 {start|stop|restart}" >&2
+	echo "Usage: $0 {start|stop|restart|stat[us]}" >&2
 	exit 1
 	;;
 esac
