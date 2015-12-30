bc05a538a3fd58763d6a674bc6a91d5ef624bc09
Sn0rt <Sn0rt@abc.shop.edu.cn>
2015-12-30 12:19:43 +0800

    import Fixed-Header-Table

---
+++ bkr-test-robot/www2/js/index.js
@@ -0,0 +1,3 @@
+$(document).ready(function() {
+    $('#table01').fixedHeaderTable({ footer: true, cloneHeadToFoot: true, altClass: 'odd', autoShow: true});
+});
--- bkr-test-robot/www2/update.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-sudo make -C ~/bkr-client-improved install_all && sudo wub-service.sh restart
