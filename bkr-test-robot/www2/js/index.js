57a9efb1348ec7d563cac6b163441e2102a319b5
tony.yin <yin-jianhong@163.com>
2016-01-04 21:46:48 +0800

    Update webUI switch to www2

--- bkr-test-robot/www2/js/index.js
+++ bkr-test-robot/www2/js/index.js
@@ -1,11 +1,26 @@
 $(document).ready(function() {
     var table = $('#table01').DataTable( {
-        scrollY:        "670px",
+        scrollY:        "600px",
         scrollX:        true,
         scrollCollapse: true,
         paging:         false,
+        columnDefs: [
+	  {
+            sortable: false,
+            "class": "index",
+            targets: 0
+	  }
+	],
+        order: [[ 1, 'asc' ]],
         fixedColumns:   {
             leftColumns: 2
         }
     } );
+
+    table.on( 'order.dt search.dt', function () {
+        table.column(0, {search:'applied', order:'applied'}).nodes().each( function (cell, i) {
+            cell.innerHTML = i+1;
+        } );
+    } ).draw();
+
 } );
