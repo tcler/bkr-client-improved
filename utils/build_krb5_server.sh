4eed0d0568830247fedba103b34ecb3b5ebcbec7
Jianhong.Yin <yin-jianhong@163.com>
2016-09-02 14:32:37 +0800

    utils: rebase build_krb5_server.sh and config_krb5_client.sh

--- utils/build_krb5_server.sh
+++ utils/build_krb5_server.sh
@@ -8,10 +8,12 @@ KRB_CONF=/etc/krb5.conf
 KDC_CONF=/var/kerberos/krb5kdc/kdc.conf
 ACL_CONF=/var/kerberos/krb5kdc/kadm5.acl
 pkgs="krb5-server krb5-workstation"
-#realm=${1:-RHQE.COM}
 realm=RHQE.COM
 hostname `hostname -A 2>/dev/null | awk '{print $1}'`
 host=$(hostname)
+ccache_name=
+keyringcache=$1
+[ -n "$keyringcache" ] && ccache_name="default_ccache_name = KEYRING:persistent:%{uid}"
 
 [ -z "$host" ] && {
 	errecho "{WARN} There is no available host:name find. use local name instead."
@@ -23,27 +25,67 @@ host=$(hostname)
 }
 
 enctypes="aes256-cts:normal aes128-cts:normal des3-hmac-sha1:normal arcfour-hmac:normal des-hmac-sha1:normal des-cbc-md5:normal des-cbc-crc:normal"
-krbConfTemp="[logging]%N default = FILE:/var/log/krb5libs.log%N kdc = FILE:/var/log/krb5kdc.log%N admin_server = FILE:/var/log/kadmind.log%N%N[libdefaults]%N dns_lookup_realm = false%N ticket_lifetime = 24h%N renew_lifetime = 7d%N forwardable = true%N rdns = false%N default_realm = EXAMPLE.COM%N%N[realms]%N EXAMPLE.COM = {%N  kdc = kerberos.example.com%N  admin_server = kerberos.example.com%N }%N%N[domain_realm]%N .example.com = EXAMPLE.COM%N example.com = EXAMPLE.COM%N"
-kdcConfTemp="[kdcdefaults]%N kdc_ports = 88%N kdc_tcp_ports = 88%N%N[realms]%N EXAMPLE.COM = {%N  #master_key_type = aes256-cts%N  acl_file = /var/kerberos/krb5kdc/kadm5.acl%N  dict_file = /usr/share/dict/words%N  admin_keytab = /var/kerberos/krb5kdc/kadm5.keytab%N  supported_enctypes = $enctypes%N }%N"
+krbConfTemp="[logging]
+ default = FILE:/var/log/krb5libs.log
+ kdc = FILE:/var/log/krb5kdc.log
+ admin_server = FILE:/var/log/kadmind.log
+
+[libdefaults]
+ dns_lookup_realm = false
+ ticket_lifetime = 24h
+ renew_lifetime = 7d
+ forwardable = true
+ rdns = false
+ default_realm = EXAMPLE.COM
+ $ccache_name
+
+[realms]
+ EXAMPLE.COM = {
+  kdc = kerberos.example.com
+  admin_server = kerberos.example.com
+ }
+
+[domain_realm]
+ .example.com = EXAMPLE.COM
+ example.com = EXAMPLE.COM"
+kdcConfTemp="[kdcdefaults]
+ kdc_ports = 88
+ kdc_tcp_ports = 88
+
+[realms]
+ EXAMPLE.COM = {
+  #master_key_type = aes256-cts
+  max_renewable_life = 7d 0h 0m 0s
+  acl_file = /var/kerberos/krb5kdc/kadm5.acl
+  dict_file = /usr/share/dict/words
+  admin_keytab = /var/kerberos/krb5kdc/kadm5.keytab
+  supported_enctypes = $enctypes
+  default_principal_flags = +renewable, +forwardable
+ }"
 kadmAclTemp="*/admin@EXAMPLE.COM	*"
 
-grep -q "$realm" "$KRB_CONF" 2>/dev/null && {
-	echo "{INFO} kerberos was ready."
+grep -q "$realm" "$KRB_CONF" && grep -q "$host" "$KRB_CONF" && grep -q "$realm" "$ACL_CONF" && {
+	echo "{INFO} Has already been configured as Kerberos Server."
+	echo "{INFO} If want to re-configure it anyway, please clear $KRB_CONF, $KDC_CONF, $ACL_CONF, etc."
+	infoecho "start KDC daemons ..."
+	chkconfig krb5kdc on; chkconfig kadmin on;
+	service krb5kdc restart || errecho "start krb5kdc failed.\n"
+	service kadmin restart  || errecho "start kadmin failed.\n"
 	exit 0
 }
+
+infoecho "make sure package $pkgs install ..."
+rpm -q $pkgs || yum -y install $pkgs
 
 infoecho "clean old config and data ..."
 #\cp $KRB_CONF ${KRB_CONF}.orig
 kdestroy -A
 \rm -f /etc/krb5.keytab  /var/kerberos/krb5kdc/principal* 
 \rm -f /tmp/krb5cc*  /var/tmp/krb5kdc_rcache  /var/tmp/rc_kadmin_0
-echo "$krbConfTemp"|sed 's/%N/\n/g' >$KRB_CONF
-echo "$kdcConfTemp"|sed 's/%N/\n/g' >$KDC_CONF
-echo "$kadmAclTemp"|sed 's/%N/\n/g' >$ACL_CONF
+echo "$krbConfTemp" >$KRB_CONF
+echo "$kdcConfTemp" >$KDC_CONF
+echo "$kadmAclTemp" >$ACL_CONF
 #===============================================================================
-
-infoecho "make sure package $pkgs install ..."
-rpm -q $pkgs || yum -y install $pkgs
 
 #infoecho "open necessary firewall port ..."
 echo <<COM
@@ -55,12 +97,21 @@ echo <<COM
 COM
 
 infoecho "close the firewall  ..."
-service iptables stop
+[ -f /etc/init.d/iptables ] && service iptables stop
 which systemctl &>/dev/null && systemctl stop firewalld
 
 infoecho "configure '$KRB_CONF' ..."
 sed -r -i -e 's;^#+;;' -e "/EXAMPLE.COM/{s//$realm/g}" -e "/kerberos.example.com/{s//$host/g}"   $KRB_CONF
 sed -r -i -e "/ (\.)?example.com/{s// \1${host#*.}/g}"   $KRB_CONF
+cat $KRB_CONF
+
+infoecho "configure '$KDC_CONF' ..."
+sed -i -e "/EXAMPLE.COM/{s//$realm/g; s/^ *#+//}"   $KDC_CONF
+cat $KDC_CONF
+
+infoecho "configure '$ACL_CONF' ..."
+sed -i -e "/EXAMPLE.COM/{s//$realm/g; s/^ *#+//}"   $ACL_CONF
+cat $ACL_CONF
 
 infoecho "create and initialize the Kerberos database on KDC"
 #( set +m; if=/dev/urandom of=/dev/random count=1024000 & )
@@ -68,8 +119,6 @@ mv  /dev/random  /dev/random.orig
 ln -s /dev/urandom  /dev/random
 (echo "redhat"; usleep 100000; echo "redhat") | kdb5_util create -r $realm -s
 mv /dev/random.orig /dev/random
-infoecho "configure '$KDC_CONF' '$ACL_CONF' ..."
-sed -i -e "/EXAMPLE.COM/{s//$realm/g; s/^ *#+//}"   $KDC_CONF $ACL_CONF
 
 infoecho "add principal root/admin and users ..."
 princList="root/admin,redhat 
@@ -93,16 +142,13 @@ infoecho "create host principal for KDC and extract to keytab ..."
 kadmin.local -q "addprinc -randkey  host/$host"
 kadmin.local -q "ktadd  host/$host"
 
-infoecho "add root and krbAccount to keytab ..."
-kadmin.local -q "ktadd  root"
-
 infoecho "========Conf finish========"
 infoecho "========Test kadmin========"
 sleep 10
-loop=10
+loop=25
 for ((i=0; i<loop; i++)); do
-	(echo -e "redhat\nlistprincs\nq\n") | kadmin && break
-	sleep 1
+	kadmin -p root/admin -w redhat -q listprincs && break
+	sleep 2
 done
 if [ $i -ge $loop ]; then
 	errecho "Test FAIL"
@@ -112,5 +158,6 @@ else
 fi
 
 infoecho "test the /etc/krb5.keytab ..."
+sleep 5
 klist -e -k -t /etc/krb5.keytab
 
--- utils/config_krb5_client.sh
+++ utils/config_krb5_client.sh
@@ -6,11 +6,30 @@ infoecho() { echo -e "\n${P:-DO ==>}" "\E[1;34m" "$@" "\E[0m"; }
 errecho() { echo -e "\n${P:-DO ==>}" "\E[31m" "$@" "\E[0m"; }
 KRB_CONF=/etc/krb5.conf
 krbServ=$1
-#realm=${2:-RHQE.COM}
 realm=RHQE.COM
 pkgs=krb5-workstation
+ccache_name=
+keyringcache=$2
+[ -n "$keyringcache" ] && ccache_name="default_ccache_name = KEYRING:persistent:%{uid}"
 
-krbConfTemp="[logging]%N default = FILE:/var/log/krb5libs.log%N kdc = FILE:/var/log/krb5kdc.log%N admin_server = FILE:/var/log/kadmind.log%N%N[libdefaults]%N dns_lookup_realm = false%N ticket_lifetime = 24h%N renew_lifetime = 7d%N forwardable = true%N rdns = false%N default_realm = EXAMPLE.COM%N%N[realms]%N EXAMPLE.COM = {%N  kdc = kerberos.example.com%N  admin_server = kerberos.example.com%N }%N%N[domain_realm]%N .example.com = EXAMPLE.COM%N example.com = EXAMPLE.COM%N"
+krbConfTemp="[libdefaults]
+ dns_lookup_realm = false
+ ticket_lifetime = 24h
+ renew_lifetime = 7d
+ forwardable = true
+ rdns = false
+ default_realm = EXAMPLE.COM
+ $ccache_name
+
+[realms]
+ EXAMPLE.COM = {
+  kdc = kerberos.example.com
+  admin_server = kerberos.example.com
+ }
+
+[domain_realm]
+ .example.com = EXAMPLE.COM
+ example.com = EXAMPLE.COM"
 
 [ $# = 0 ] && {
 	echo "usage: $0  <krb5 server address>"
@@ -30,26 +49,27 @@ echo -e "\[" | telnet $krbServ 88 2>&1 | egrep 'Escape character is' || {
 	exit 1
 }
 
+infoecho "make sure package $pkgs install ..."
+rpm -q $pkgs || yum -y install $pkgs
+
 if test "$HOSTNAME" != "$krbServ"; then
 	infoecho "clean old config and data ..."
 	kdestroy -A
 	\rm -f /etc/krb5.keytab
 	\rm -f /tmp/krb5cc*  /var/tmp/krb5kdc_rcache  /var/tmp/rc_kadmin_0
-	echo "$krbConfTemp"|sed 's/%N/\n/g' >$KRB_CONF
+	echo "$krbConfTemp" >$KRB_CONF
 else
-	grep -q "$realm" "$KRB_CONF" && infoecho "$KRB_CONF has configed ..."
+	infoecho "$KRB_CONF has configed ..."
 fi
-
-infoecho "make sure package $pkgs install ..."
-rpm -q $pkgs || yum -y install $pkgs
 #===============================================================================
 infoecho "close the firewall ..."
-service iptables stop
+[ -f /etc/init.d/iptables ] && service iptables stop
 which systemctl &>/dev/null && systemctl stop firewalld
 
 infoecho "configure '$KRB_CONF', edit the realm name ..."
 sed -r -i -e 's;^#+;;' -e "/EXAMPLE.COM/{s//$realm/g}" -e "/kerberos.example.com/{s//$krbServ/g}"   $KRB_CONF
 sed -r -i -e "/ (\.)?example.com/{s// \1${krbServ#*.}/g}"   $KRB_CONF
+cat $KRB_CONF
 
 infoecho "test connect to KDC with kadmin ..."
 kadmin -p root/admin -w redhat -q "listprincs"
@@ -60,14 +80,16 @@ nfsprinc=nfs/${host}
 kadmin -p root/admin -w redhat -q "addprinc -randkey $nfsprinc"
 kadmin -p root/admin -w redhat -q "ktadd -k /etc/krb5.keytab $nfsprinc"
 
-infoecho "create root@ and krbAccount@ princs and ext keytab ..."
-klist -e -k -t /etc/krb5.keytab|grep -q 'root@' ||
-    kadmin -p root/admin -w redhat -q "ktadd -k /etc/krb5.keytab root"
+infoecho "create cifs princs and ext keytab ..."
+cifsprinc=cifs/${host}
+kadmin -p root/admin -w redhat -q "addprinc -pw redhat  $cifsprinc"
+kadmin -p root/admin -w redhat -q "ktadd $cifsprinc"
 
 infoecho "test the /etc/krb5.keytab ..."
+sleep 5
 klist -e -k -t /etc/krb5.keytab
 
 infoecho "fetch tickit: kinit -k  ..."
-kinit -k $nfsprinc &&
-	kinit -k root
-
+kinit -k $cifsprinc
+kinit -k $nfsprinc
+echo redhat | kinit root
