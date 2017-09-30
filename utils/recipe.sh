39f9874e42a0874353ce54ec26f14468b3abb0ca
Jianhong.Yin <yin-jianhong@163.com>
2017-09-30 09:46:52 +0800

    recipe.sh: fix config file name, and add data time check

--- utils/recipe.sh
+++ utils/recipe.sh
@@ -6,7 +6,7 @@
 declare -A HostMax
 
 eval "$(tclsh <<EOF
-$(cat /etc/bkr-client-improved/bkr-autorun.conf ~/.bkr-client-improved/bkr-autorun.confx 2>/dev/null)
+$(cat /etc/bkr-client-improved/bkr-autorun.conf ~/.bkr-client-improved/bkr-autorun.conf 2>/dev/null)
 puts "
 HostMax\[x86_64\]=\$HostAvilable(x86_64)
 HostMax\[i386\]=\$HostAvilable(i386)
@@ -26,6 +26,13 @@ recipeCheck() {
 	local user=${2:-jiyin}
 	local url=http://nest.test.redhat.com/mnt/qa/scratch/azelinka/beaker-stats/top-recipe-owners.$arch
 	I=$(echo $url; curl $url 2>/dev/null)
+	T=$(echo "$I" | tail -n1)
+	read Date Hour Min Sec ignore <<<"${T//[:.]/ }"
+	[[ $Date = $(date +%F) && $Hour = $(date +%H) ]] || {
+		echo "[$(date +%F_%T)] Data time is wrong $Date $Hour:$Min:$Sec "
+		return 0
+	}
+
 	M=$(echo "$I" | awk '$1 ~ "'$user'" {print $1, $2}')
 	while read U N; do
 		[ "${N:-0}" -gt ${HostMax[$arch]} ] && {
