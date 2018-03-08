3e127baa20af3abaf44f604c19030cfbad652b2d
Jianhong.Yin <yin-jianhong@163.com>
2018-03-08 23:21:43 +0800

    bkr-system-broken-recover.sh: add options for email argument

--- cron_task/bkr-system-broken-recover.sh
+++ cron_task/bkr-system-broken-recover.sh
@@ -1,7 +1,26 @@
 #!/bin/bash
 
-owner=$1
+Usage() {
+	echo "Usage: $0 [-o owner] [-f emailFrom] [-c emailCc]" >&2
+}
+
+_at=`getopt -o ho:c: \
+	--long help \
+	--long owner: \
+    -n "$0" -- "$@"`
+eval set -- "$_at"
+while true; do
+	case "$1" in
+	-h|--help)   Usage; shift 1; exit 0;;
+	-o|--owner)  owner=$2; shift 2;;
+	-f)          from=$2; shift 2;;
+	-c)          cc=$2; shift 2;;
+	--) shift; break;;
+	esac
+done
+
 owner=${owner:-fs-qe}
+from=${from:-QE Assistant <jiyin@redhat.com>}
 
 brokenList=$(bkr list-systems --xml-filter='<system><owner op="==" value="'"$owner"'"/></system>' --status Broken)
 
@@ -14,7 +33,7 @@ for h in $brokenList; do
 
 		# send e-mail
 		read key user < <(grep LoanedTo: <<<"$hinfo")
-		cat <<-EOF | sendmail.sh -p '[Notice] ' -f "QE-Assistant <jiyin@redhat.com>" -t "${owner}@redhat.com" -c "${user}@redhat.com" - "Broken host remind"  &>/dev/null
+		cat <<-EOF | sendmail.sh -p '[Notice] ' -f "$from" -t "${user}@redhat.com" -c "$cc" - "Broken host remind"  &>/dev/null
 		Hi ${user}
 
 		host $h loaned to you was broken, I have recovered it to 'Manual'
