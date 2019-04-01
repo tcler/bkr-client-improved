6c81992a8db58991f8592b01d78e2fe6c10fdba8
Jianhong.Yin <yin-jianhong@163.com>
2019-04-01 17:18:45 +0800

    utils/mntbind.sh: change the order of mount dev and mountpoint
    Signed-off-by: Jianhong Yin <yin-jianhong@163.com>

--- utils/mntbind.sh
+++ utils/mntbind.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
+# now linux doesn't support mount -t bind
+# so here you are
 
 export LANG=C
 while read dst src drop; do
 	dev=${src%\[/*}
 	rpath=${src##*\[/}; rpath=${rpath%]}
 	rootdir=$(findmnt --list | awk -v dev=$dev '$2 == dev && $1 != "/" {print $1}')
-	echo $dst $'\t' ${rootdir}/${rpath/\/\/deleted/ #-deleted}
+	echo ${rootdir}/${rpath/\/\/deleted/ #-deleted} $'\t' $dst
 done < <(findmnt --list | awk '$2 ~ /\]$/')
