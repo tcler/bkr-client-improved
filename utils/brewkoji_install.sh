67260c3bfa51a3da017f3d0a8c020544b6204a42
Jianhong.Yin <yin-jianhong@163.com>
2019-10-17 15:21:20 +0800

    brewkoji_install.sh: add CentOS support and update url
    Signed-off-by: Jianhong Yin <yin-jianhong@163.com>

--- utils/brewkoji_install.sh
+++ utils/brewkoji_install.sh
@@ -3,6 +3,7 @@
 
 installBrew2() {
 	#https://mojo.redhat.com/docs/DOC-1024827
+	#https://docs.engineering.redhat.com/display/RCMDOC/RCM+Tools+Release+Guide
 	urls="
 	curl -L -O http://download.devel.redhat.com/rel-eng/RCMTOOLS/rcm-tools-rhel-6-server.repo
 	curl -L -O http://download.devel.redhat.com/rel-eng/RCMTOOLS/rcm-tools-rhel-6-client.repo
@@ -19,9 +20,15 @@ installBrew2() {
 
 	pushd /etc/yum.repos.d
 	case $name in
-	RedHatEnterprise*)
+	RedHatEnterprise*|CentOS*)
 		case $verx in
-		6|7)
+		6)
+			for type in server client workstation; do
+				curl -L -O http://download.devel.redhat.com/rel-eng/RCMTOOLS/rhel-${verx}/rcm-tools-rhel-${verx}-${type}.repo
+				yum install -y koji brewkoji && break || rm rcm-tools-*.repo
+			done
+			;;
+		7)
 			for type in server client workstation; do
 				curl -L -O http://download.devel.redhat.com/rel-eng/RCMTOOLS/rcm-tools-rhel-${verx}-${type}.repo
 				yum install -y koji brewkoji && break || rm rcm-tools-*.repo
