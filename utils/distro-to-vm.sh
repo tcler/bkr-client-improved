63f2a18b234dad11f66e1a75df9974de91f0611c
Jianhong.Yin <yin-jianhong@163.com>
2019-10-23 11:07:08 +0800

    distro-to-vm.sh: support install by import type
    Signed-off-by: Jianhong Yin <yin-jianhong@163.com>

--- utils/distro-to-vm.sh
+++ utils/distro-to-vm.sh
@@ -3,18 +3,21 @@ LANG=C
 
 Distro=
 Location=
+Imageurl=
 VM_OS_VARIANT=
 OVERWRITE=no
 KSPath=
 ksauto=
 MacvtapMode=vepa
 VMName=
+InstallType=location
 
 Usage() {
 	cat <<-EOF >&2
 	Usage:
 	 $0 <[-d] distroname> [-ks ks-file] [-l location] [-osv variant] [-macvtap {vepa|bridge}] [-f|-force] [-n|-vmname name]
 	 $0 <[-d] distroname> [options..] [-b|-brewinstall <arg>] [-g|-genimage]
+	 $0 <[-d] distroname> <-I> [-rm] # install by using import method
 
 	Example Internet:
 	 $0 centos-5 -l http://vault.centos.org/5.11/os/x86_64/
@@ -39,9 +42,10 @@ Usage() {
 	EOF
 }
 
-_at=`getopt -o hd:l:fn:gb: \
+_at=`getopt -o hd:l:fn:gb:I:: \
 	--long help \
 	--long ks: \
+	--long rm \
 	--long osv: \
 	--long os-variant: \
 	--long force \
@@ -58,7 +62,9 @@ while true; do
 	-h|--help) Usage; shift 1; exit 0;;
 	-d)        Distro=$2; shift 2;;
 	-l)        Location=$2; shift 2;;
+	-I)        InstallType=import; Imageurl=${2#=}; shift 2;;
 	--ks)      KSPath=$2; shift 2;;
+	--rm)      RM=yes; shift 1;;
 	--xzopt)         XZ="$2"; shift 2;;
 	--macvtap)       MacvtapMode="$2"; shift 2;;
 	-f|--force)      OVERWRITE="yes"; shift 1;;
@@ -100,11 +106,9 @@ distro2location() {
 }
 
 distro2imageurl() {
-	local distro=$1
-	local variant=${2:-Server}
-
+	local osurl=$1
 	local rc=1
-	local osurl=$(distro2location $distro)
+
 	local imageurl=${osurl/\/os\//\/images\/}
 	local imagename=$(curl -s ${imageurl} | sed -nr '/.*>(rhel-[^<>]+qcow2)<.*/{s//\1/;p}')
 	if [[ -n "${imagename}" ]]; then
@@ -120,84 +124,89 @@ distro2imageurl() {
 	exit 1
 }
 
-[[ -n "$GetImage" ]] && {
-	distro2imageurl $Distro
-	exit $rc
-}
-
-echo "{INFO} guess/verify os-variant ..."
-#Speculate the appropriate VM_OS_VARIANT according distro name
-[[ -z "$VM_OS_VARIANT" ]] && {
-	VM_OS_VARIANT=${Distro/-/}
-	VM_OS_VARIANT=${VM_OS_VARIANT%%-*}
-	VM_OS_VARIANT=${VM_OS_VARIANT,,}
-}
-osvariants=$(virt-install --os-variant list 2>/dev/null) ||
-	osvariants=$(osinfo-query os 2>/dev/null)
-[[ -n "$osvariants" ]] && {
-	grep -q "^ $VM_OS_VARIANT " <<<"$osvariants" || VM_OS_VARIANT=${VM_OS_VARIANT/.*/-unknown}
-	grep -q "^ $VM_OS_VARIANT " <<<"$osvariants" || VM_OS_VARIANT=${VM_OS_VARIANT/[0-9]*/-unknown}
-	if grep -q "^ $VM_OS_VARIANT " <<<"$osvariants"; then
-		OS_VARIANT_OPT=--os-variant=$VM_OS_VARIANT
-	fi
-}
-
-[[ -z "$Location" ]] && {
-	echo "{INFO} trying to get distro location according distro name($Distro) ..."
-	Location=$(distro2location $Distro)
-
-	[[ -z "$Location" ]] && {
-		echo "{WARN} can not get distro location. please check if '$Distro' is valid distro" >&2
-		exit
-	}
-}
-
 vmname=${Distro//./}
 vmname=${vmname,,}
 [[ -n "$VMName" ]] && vmname=${VMName}
-[[ -z "$KSPath" ]] && {
-	ksauto=/tmp/ks-$VM_OS_VARIANT-$$.cfg
-	KSPath=$ksauto
-	which ks-generator.sh &>/dev/null || {
-		_url=https://raw.githubusercontent.com/tcler/bkr-client-improved/master/utils/ks-generator.sh
-		mkdir -p ~/bin && wget -O ~/bin/ks-generator.sh -N -q $_url
-		chmod +x ~/bin/ks-generator.sh
-	}
-	ks-generator.sh -d $Distro -url $Location >$KSPath
 
-	[[ -n "$PKGS" ]] && {
-		cat <<-END >>$KSPath
-		%post --log=/root/my-ks-post.log
-		wget -N -q https://raw.githubusercontent.com/tcler/bkr-client-improved/master/utils/brewinstall.sh
-		chmod +x brewinstall.sh
-		./brewinstall.sh $PKGS
-		%end
-		END
-	}
+if [[ "$InstallType" = import || -n "$GetImage" ]]; then
+	if [[ -z "$Imageurl" ]]; then
+		echo "{INFO} getting fastest location of $Distro ..." >&2
+		Location=$(distro2location $Distro)
+		[[ -z "$Location" ]] && {
+			echo "{WARN} can not find location info of '$Distro'" >&2
+			exit 1
+		}
+		Imageurl=$(distro2imageurl $Location)
+		if [[ $? = 0 ]]; then
+			[[ -n "$GetImage" ]] && { echo "$Imageurl"; exit; }
+		else
+			[[ -n "$GetImage" ]] && { exit 1; }
+			echo "{INFO} can not find image info of '$Distro', switching to Location mode" >&2
+			InstallType=location
+		fi
+	fi
+elif [[ "$InstallType" = location ]]; then
+	if [[ -z "$Location" ]]; then
+		Location=$(distro2location $Distro)
+		[[ -z "$Location" ]] && {
+			echo "{WARN} can not find distro location. please check if '$Distro' is valid distro" >&2
+			exit 1
+		}
+	fi
+fi
 
-	sed -i "/^%post/s;$;\ntest -f /etc/hostname \&\& echo ${vmname} >/etc/hostname || echo HOSTNAME=${vmname} >>/etc/sysconfig/network;" $KSPath
-	[[ "$Distro" =~ (RHEL|centos)-?5 ]] && {
-		ex -s $KSPath <<-EOF
-		/%packages/,/%end/ d
-		$ put
-		$ d
-		w
-		EOF
-	}
-}
+if [[ "$InstallType" = location ]]; then
+	[[ -z "$KSPath" ]] && {
+		echo "{INFO} generating kickstart file for $Distro ..."
+		ksauto=/tmp/ks-$VM_OS_VARIANT-$$.cfg
+		KSPath=$ksauto
+		which ks-generator.sh &>/dev/null || {
+			_url=https://raw.githubusercontent.com/tcler/bkr-client-improved/master/utils/ks-generator.sh
+			mkdir -p ~/bin && wget -O ~/bin/ks-generator.sh -N -q $_url
+			chmod +x ~/bin/ks-generator.sh
+		}
+		ks-generator.sh -d $Distro -url $Location >$KSPath
 
-echo -e "{INFO} install libvirt service and related packages ..."
+		[[ -n "$PKGS" ]] && {
+			cat <<-END >>$KSPath
+			%post --log=/root/my-ks-post.log
+			wget -N -q https://raw.githubusercontent.com/tcler/bkr-client-improved/master/utils/brewinstall.sh
+			chmod +x brewinstall.sh
+			./brewinstall.sh $PKGS
+			%end
+			END
+		}
+
+		sed -i "/^%post/s;$;\ntest -f /etc/hostname \&\& echo ${vmname} >/etc/hostname || echo HOSTNAME=${vmname} >>/etc/sysconfig/network;" $KSPath
+		[[ "$Distro" =~ (RHEL|centos)-?5 ]] && {
+			ex -s $KSPath <<-EOF
+			/%packages/,/%end/ d
+			$ put
+			$ d
+			w
+			EOF
+		}
+	}
+elif [[ "$InstallType" = import ]]; then
+	:
+fi
+
+echo -e "{INFO} check/install libvirt service and related packages ..."
 sudo yum install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm &>/dev/null
 sudo yum install -y libvirt libvirt-client virt-install virt-viewer qemu-kvm expect nmap-ncat nc libguestfs-tools-c &>/dev/null
 
-echo -e "{INFO} install libvirt-nss module ..."
+echo -e "{INFO} check/install libvirt-nss module ..."
 sudo yum install -y libvirt-nss &>/dev/null
 grep -q '^hosts:.*libvirt libvirt_guest' /etc/nsswitch.conf ||
 	sed -i '/^hosts:/s/files /&libvirt libvirt_guest /' /etc/nsswitch.conf
 
-echo -e "{INFO} creating VM by using location:\n  $Location"
+echo -e "{INFO} start libvirtd/virtlogd service ..."
+service libvirtd start
+service virtlogd start
+
 virsh desc $vmname &>/dev/null && {
 	if [[ "${OVERWRITE}" = "yes" ]]; then
+		echo "{INFO} VM $vmname has been there, remove it ..."
 		file=$(virsh dumpxml --domain $vmname | sed -n "/source file=/{s|^.*='||; s|'/>$||; p}")
 		virsh destroy $vmname
 		virsh undefine $vmname --remove-all-storage
@@ -207,31 +216,6 @@ virsh desc $vmname &>/dev/null && {
 		[[ -n "$ksauto" ]] && \rm -f $ksauto
 		exit
 	fi
-}
-
-service libvirtd start
-service virtlogd start
-
-is_bridge() {
-	local ifname=$1
-	[[ -z "$ifname" ]] && return 1
-	ip -d a s $ifname | grep -qw bridge
-}
-get_default_if() {
-	local notbr=$1
-	local iface=
-
-	iface=$(ip route get 1 | awk '/^[0-9]/{print $5}')
-	if [[ -n "$notbr" ]] && is_bridge $iface; then
-		# ls /sys/class/net/$iface/brif
-		if command -v brctl; then
-			brctl show $iface | awk 'NR==2 {print $4}'
-		else
-			ip link show type bridge_slave | awk -F'[ :]+' '/master '$iface' state UP/{print $2}' | head -n1
-		fi
-		return 0
-	fi
-	echo $iface
 }
 
 : <<COMM
@@ -257,76 +241,196 @@ virsh net-start subnet10
 virsh net-autostart subnet10
 COMM
 
+is_bridge() {
+	local ifname=$1
+	[[ -z "$ifname" ]] && return 1
+	ip -d a s $ifname | grep -qw bridge
+}
+get_default_if() {
+	local notbr=$1
+	local iface=
+
+	iface=$(ip route get 1 | awk '/^[0-9]/{print $5}')
+	if [[ -n "$notbr" ]] && is_bridge $iface; then
+		# ls /sys/class/net/$iface/brif
+		if command -v brctl; then
+			brctl show $iface | awk 'NR==2 {print $4}'
+		else
+			ip link show type bridge_slave | awk -F'[ :]+' '/master '$iface' state UP/{print $2}' | head -n1
+		fi
+		return 0
+	fi
+	echo $iface
+}
+
+echo "{INFO} guess/verify os-variant ..."
+#Speculate the appropriate VM_OS_VARIANT according distro name
+[[ -z "$VM_OS_VARIANT" ]] && {
+	VM_OS_VARIANT=${Distro/-/}
+	VM_OS_VARIANT=${VM_OS_VARIANT%%-*}
+	VM_OS_VARIANT=${VM_OS_VARIANT,,}
+}
+osvariants=$(virt-install --os-variant list 2>/dev/null) ||
+	osvariants=$(osinfo-query os 2>/dev/null)
+[[ -n "$osvariants" ]] && {
+	grep -q "^ $VM_OS_VARIANT " <<<"$osvariants" || VM_OS_VARIANT=${VM_OS_VARIANT/.*/-unknown}
+	grep -q "^ $VM_OS_VARIANT " <<<"$osvariants" || VM_OS_VARIANT=${VM_OS_VARIANT/[0-9]*/-unknown}
+	if grep -q "^ $VM_OS_VARIANT " <<<"$osvariants"; then
+		OS_VARIANT_OPT=--os-variant=$VM_OS_VARIANT
+	fi
+}
+
+echo -e "{INFO} get available vnc port ..."
 VNCPORT=${VNCPORT:-7777}
 while nc 127.0.0.1 ${VNCPORT} </dev/null &>/dev/null; do
 	let VNCPORT++
 done
 
-[[ "$GenerateImage" = yes ]] && {
-	sed -i '/^reboot$/s//poweroff/' ${KSPath}
-	NOREBOOT=--noreboot
-}
-ksfile=${KSPath##*/}
-virt-install --connect=qemu:///system --hvm --accelerate \
-  --name $vmname \
-  --location $Location \
-  $OS_VARIANT_OPT \
-  --memory 2048 \
-  --vcpus 2 \
-  --disk size=8 \
-  --network network=default \
-  --network type=direct,source=$(get_default_if notbr),source_mode=$MacvtapMode \
-  --initrd-inject $KSPath \
-  --extra-args="ks=file:/$ksfile console=tty0 console=ttyS0,115200n8" \
-  --vnc --vnclisten 0.0.0.0 --vncport ${VNCPORT} $NOREBOOT &
-installpid=$!
-sleep 5s
+if [[ "$InstallType" = location ]]; then
+	echo -e "{INFO} creating VM by using location:\n  $Location"
+	[[ "$GenerateImage" = yes ]] && {
+		sed -i '/^reboot$/s//poweroff/' ${KSPath}
+		NOREBOOT=--noreboot
+	}
+	ksfile=${KSPath##*/}
+	virt-install --connect=qemu:///system --hvm --accelerate \
+	  --name $vmname \
+	  --location $Location \
+	  $OS_VARIANT_OPT \
+	  --memory 2048 \
+	  --vcpus 2 \
+	  --disk size=8 \
+	  --network network=default \
+	  --network type=direct,source=$(get_default_if notbr),source_mode=$MacvtapMode \
+	  --initrd-inject $KSPath \
+	  --extra-args="ks=file:/$ksfile console=tty0 console=ttyS0,115200n8" \
+	  --vnc --vnclisten 0.0.0.0 --vncport ${VNCPORT} $NOREBOOT &
+	installpid=$!
+	sleep 5s
 
-while ! virsh desc $vmname &>/dev/null; do test -d /proc/$installpid || exit 1; sleep 1s; done
-for ((i=0; i<8; i++)); do
-	#clear -x
-	printf '\33[H\33[2J'
-	expect -c '
-		set timeout -1
-		spawn virsh console '"$vmname"'
-		expect {
-			"error: Disconnected from qemu:///system due to end of file*" {
-				send "\r"
-				puts $expect_out(buffer)
-				exit 5
+	while ! virsh desc $vmname &>/dev/null; do test -d /proc/$installpid || exit 1; sleep 1s; done
+	for ((i=0; i<8; i++)); do
+		#clear -x
+		printf '\33[H\33[2J'
+		expect -c '
+			set timeout -1
+			spawn virsh console '"$vmname"'
+			expect {
+				"error: Disconnected from qemu:///system due to end of file*" {
+					send "\r"
+					puts $expect_out(buffer)
+					exit 5
+				}
+				"error: The domain is not running" {
+					send "\r"
+					puts $expect_out(buffer)
+					exit 6
+				}
+				"error: internal error: character device console0 is not using a PTY" {
+					send "\r"
+					puts $expect_out(buffer)
+					exit 7
+				}
+				"* login:" {
+					send "\r"
+					exit 0
+				}
+				"Unsupported Hardware Detected" {
+					send "\r"
+				}
+				"Which would you like to install through" {
+					# see: [RHEL 6.1] Anaconda requires user interaction in case of kickstart network activation failing
+					send "\r"
+				}
 			}
-			"error: The domain is not running" {
-				send "\r"
-				puts $expect_out(buffer)
-				exit 6
-			}
-			"error: internal error: character device console0 is not using a PTY" {
-				send "\r"
-				puts $expect_out(buffer)
-				exit 7
-			}
-			"* login:" {
-				send "\r"
-				exit 0
-			}
-			"Unsupported Hardware Detected" {
-				send "\r"
-			}
-			"Which would you like to install through" {
-				# see: [RHEL 6.1] Anaconda requires user interaction in case of kickstart network activation failing
-				send "\r"
-			}
-		}
-		interact
-	' && break
-	test -d /proc/$installpid || break
-	sleep 2
-done
+			interact
+		' && break
+		test -d /proc/$installpid || break
+		sleep 2
+	done
 
-# waiting install finish ...
-echo -e "{INFO} waiting install finish ..."
-while test -d /proc/$installpid; do sleep 5; done
-[[ -n "$ksauto" ]] && \rm -f $ksauto
+	# waiting install finish ...
+	echo -e "{INFO} waiting install finish ..."
+	while test -d /proc/$installpid; do sleep 5; done
+	[[ -n "$ksauto" ]] && \rm -f $ksauto
+
+elif [[ "$InstallType" = import ]]; then
+	echo "{INFO} downloading cloud image file of $Distro ..."
+	wget -N $Imageurl
+	[[ -f ${Imageurl##*/} ]] || exit 1
+	sed -i '/^#(user|group) =/s/^#//' /etc/libvirt/qemu.conf;
+
+	imagefile=${Imageurl##*/}
+	[[ $imagefile = *.xz ]] && {
+		xz -d $imagefile
+		imagefile=${imagefile%.xz}
+	}
+
+	echo -e "{INFO} creating cloud-init iso"
+	tmpdir=.tmp$$
+	mkdir -p $tmpdir
+	pushd $tmpdir
+	echo "local-hostname: mylinux.local" >meta-data
+	cat >user-data <<-EOF
+	#cloud-config
+	users:
+	  - default
+
+	  - name: root
+	    plain_text_passwd: redhat
+	    lock_passwd: false
+
+	  - name: foo
+	    group: sudo
+	    plain_text_passwd: redhat
+	    lock_passwd: false
+
+	chpasswd: { expire: False }
+	EOF
+	genisoimage -output ../cloud-init-$$.iso -volid cidata -joliet -rock user-data meta-data
+	popd
+	rm -rf $tmpdir
+
+	echo -e "{INFO} creating VM by import $imagefile"
+	virt-install \
+	  --name $vmname \
+	  --memory 2048 \
+	  --vcpus 2 \
+	  --disk $imagefile \
+	  --disk cloud-init-$$.iso,device=cdrom \
+	  --import \
+	  $OS_VARIANT_OPT &
+
+	installpid=$!
+	sleep 5s
+	for ((i=0; i<8; i++)); do
+		expect -c '
+			set timeout -1
+			spawn virsh console '"$vmname"'
+			expect {
+				"error: internal error: character device console0 is not using a PTY" {
+					send "\r"
+					puts $expect_out(buffer)
+					exit 7
+				}
+				"* login:" { send "root\r" }
+			}
+			expect "Password:" {
+				send "redhat\r"
+				send "# your are in console, Ctr + ] to exit \r"
+			}
+			interact
+		' && break
+		test -d /proc/$installpid || break
+		sleep 2
+	done
+	[[ "$RM" = yes ]] && {
+		virsh destroy $vmname
+		sleep 2
+		virsh undefine $vmname --remove-all-storage
+	}
+	exit
+fi
 
 if [[ "$GenerateImage" = yes ]]; then
 	image=$(virsh dumpxml --domain $vmname | sed -n "/source file=/{s|^.*='||; s|'/>$||; p}")
