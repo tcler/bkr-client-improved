218eb1c9d7456d71497c51b14c3d135adbc75e98
Jianhong.Yin <yin-jianhong@163.com>
2019-10-31 14:33:06 +0800

    vmcreate: fix sshkey file path
    and add -D option for cloud-init-iso-gen.sh
    
    Signed-off-by: Jianhong Yin <yin-jianhong@163.com>

--- utils/cloud-init-iso-gen.sh
+++ utils/cloud-init-iso-gen.sh
@@ -7,7 +7,7 @@ Repos=()
 BPKGS=
 PKGS=
 
-_at=`getopt -o hp:b: \
+_at=`getopt -o hp:b:D \
 	--long help \
 	--long hostname: \
 	--long repo: \
@@ -19,6 +19,7 @@ eval set -- "$_at"
 while true; do
 	case "$1" in
 	-h|--help) Usage; shift 1; exit 0;;
+	-D) Debug=yes; shift 1;;
 	--hostname) HostName="$2"; shift 2;;
 	--repo) Repos+=($2); shift 2;;
 	-p|--pkginstall) PKGS="$2"; shift 2;;
@@ -102,4 +103,5 @@ genisoimage -output $isof -volid cidata -joliet -rock user-data meta-data
 
 popd &>/dev/null
 
+[[ -n "$Debug" ]] && cat $tmpdir/*
 rm -rf $tmpdir
