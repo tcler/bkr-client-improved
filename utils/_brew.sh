20a3ae90965f6cc31b47adb641413bd97aa63314
Jianhong.Yin <yin-jianhong@163.com>
2019-11-20 10:53:46 +0800

    utils: remove commands: downloadBrewBuild installBrewPkg
    please use follow command line stead
    $ brewinstall.sh -onlydownload [args]
    $ brewinstall.sh [args]
    
    Signed-off-by: Jianhong Yin <yin-jianhong@163.com>

--- utils/_brew.sh
+++ utils/_brew.sh
@@ -26,10 +26,6 @@ which brew &>/dev/null || {
 	}
 }
 
-verx=$(lsb_release -sr|awk -F. '{print $1}')
-dfix=f
-echo "$(lsb_release -si)" | egrep -q "^(RedHat|Sci|Cent)" && dfix=el
-
 progname=${0##*/}
 
 _searchBrewBuild() {
@@ -40,13 +36,6 @@ _searchBrewBuild() {
 	local pattern=$1
 	brew search build $dbgArg -r "$pattern" | sort -V -r
 }
-_downloadBrewBuild() {
-	[ $# -lt 1 ] && {
-		echo "usage: $0 <pkgName> [--arch=<src|i386|ppc64|...>]" >&2
-		return 1
-	}
-	brew  $dbgArg download-build "$@"
-}
 _stateBrewBuild() {
 	[ $# -lt 1 ] && {
 		echo "usage: $0 <pkgName>" >&2
@@ -55,65 +44,11 @@ _stateBrewBuild() {
 	local nvr=$1
 	brew buildinfo $nvr | awk '/State/ {print $2}'
 }
-_installBrewPkg() {
-Usage() {
-	echo "usage: $0 [-f] [-n] <pkgNamePattern> [pkgNamePattern ...]"
-	echo "   ex: $0 ^autofs-[0-9].*"
-	echo "   ex: $0 -n ^autofs-[0-9].*\.el7"
-	echo "   -f	Force install package"
-	echo "   -n	Not append distroVersion info at search pattern"
-	echo "   --debuginfo	install the debuginfo pkg"
-	return 1
-}
-	TEMP=`getopt -o "fna:" --long debuginfo --long target   -n 'example.bash' -- "$@"`
-	if [ $? != 0 ] ; then Usage >&2 ; exit 1 ; fi
-
-	# Note the quotes around `$TEMP': they are essential!
-	eval set -- "$TEMP"
-	#===============================================================================
-	autofix='.*\.'"${dfix}${verx}"
-	# parse the argument
-	OPTIND=1
-	local arch=$(arch)
-	while true ; do
-		case $1 in
-		-f)	force="--force --nodeps"; shift 1;;
-		-n)	autofix=; shift 1;;
-		-a)	arch=$2; shift 2;;
-		--debuginfo)	debuginfo=--debuginfo; shift 1;;
-		--)	shift; break;;
-		*) echo "Internal error!"; exit 1;;
-		esac
-	done
-	[ $# -lt 1 ] && { Usage >&2; exit 1; }
-	test $arch = i686 && vercmp $(lsb_release -sr) match '^5\..*' && arch=i386
-	local list="$@"
-	[ -n "${BrewPkgList}" ] && list=${BrewPkgList}
-
-	local dir=$(mktemp -d)
-	pushd $dir
-	for pkg in ${list}; do
-		local bname=`_searchBrewBuild "${pkg}${autofix}" | head -n 1`
-		[ -z "$bname" ] && echo "[Warn] brew search fail in '${pkg}${autofix#\\}'"
-		_downloadBrewBuild $bname --arch=$arch $debuginfo || _downloadBrewBuild $bname --arch=noarch
-	done
-	ls *.rpm &>/dev/null && {
-		rpm -ivh *.rpm $force || yum install -y *.rpm
-	}
-	popd
-	rm -rf $dir
-}
 
 case $progname in
 searchBrewBuild)
 	_searchBrewBuild "$@" ;;
-downloadBrewBuild)
-	_downloadBrewBuild "$@" ;;
 stateBrewBuild)
 	_stateBrewBuild "$@" ;;
-installBrewPkg)
-	_installBrewPkg "$@" ;;
-*)
-	brew "$@" ;;
 esac
 
