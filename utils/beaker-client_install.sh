8dfb382024121c5fec390e2711786ddc67c43fcf
Jianhong.Yin <yin-jianhong@163.com>
2019-12-09 13:57:18 +0800

    beaker-client_install.sh: fix baseurl on Fedora
    Signed-off-by: Jianhong Yin <yin-jianhong@163.com>

--- utils/beaker-client_install.sh
+++ utils/beaker-client_install.sh
@@ -8,13 +8,23 @@ rpm -q beaker-client && {
 #__main__
 kuser=$1
 
-cat <<'EOF' >/etc/yum.repos.d/beaker-client.repo
-[beaker-client]
-name=Beaker Client - RedHatEnterpriseLinux$releasever
-baseurl=http://download.lab.bos.redhat.com/beakerrepos/client/RedHatEnterpriseLinux$releasever/
-enabled=1
-gpgcheck=0
-EOF
+if grep -q NAME=Fedora /etc/os-release; then
+	cat <<-'EOF' >/etc/yum.repos.d/beaker-client.repo
+	[beaker-client]
+	name=Beaker Client - Fedora$releasever
+	baseurl=http://download.lab.bos.redhat.com/beakerrepos/client/Fedora$releasever/
+	enabled=1
+	gpgcheck=0
+	EOF
+else
+	cat <<-'EOF' >/etc/yum.repos.d/beaker-client.repo
+	[beaker-client]
+	name=Beaker Client - RedHatEnterpriseLinux$releasever
+	baseurl=http://download.lab.bos.redhat.com/beakerrepos/client/RedHatEnterpriseLinux$releasever/
+	enabled=1
+	gpgcheck=0
+	EOF
+fi
 
 yum install -y beaker-client
 
