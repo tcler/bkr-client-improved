cf90d2da756aa448c434e0cba45a0f1944d2f8de
Jianhong.Yin <yin-jianhong@163.com>
2019-12-31 13:49:07 +0800

    utils/srcrpmbuild.sh: add second parameter $buildtype
    Signed-off-by: Jianhong Yin <yin-jianhong@163.com>

--- utils/srcrpmbuild.sh
+++ utils/srcrpmbuild.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
 
+which rpmbuild &>/dev/null || yum install -y /usr/bin/rpmbuild
+
 pkg=$1
+buildtype=${2:-p}
+
+[[ "$buildtype" = b ]] && {
+	which gcc &>/dev/null || yum install -y gcc
+}
 
 mkdir -p ~/rpm/build
 rm -rf ~/rpmbuild/*
 rpm -ivh $pkg
-rpmbuild -bp ~/rpmbuild/SPECS/*.spec --nodeps
-mv ~/rpmbuild/BUILD/* .
+rpmbuild -b${buildtype} ~/rpmbuild/SPECS/*.spec
+
+if [[ "$buildtype" = p ]]; then
+	mv -f ~/rpmbuild/BUILD/* ./.
+elif [[ "$buildtype" = b ]]; then
+	mv -f ~/rpmbuild/RPMS/x86_64/* ./.
+fi
