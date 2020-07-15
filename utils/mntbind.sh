311789b145c5c3c5a37f7e7e5e080415b1277b6d
Jianhong Yin <jiyin@redhat.com>
2020-07-16 07:16:05 +0800

    utils/mntbind.sh: optimize code
    Signed-off-by: Jianhong Yin <yin-jianhong@163.com>

--- utils/mntbind.sh
+++ utils/mntbind.sh
@@ -3,9 +3,9 @@
 # so here you are
 
 export LANG=C
-while read dst src drop; do
+while read dst src others; do
 	dev=${src%\[/*}
 	rpath=${src##*\[/}; rpath=${rpath%]}
 	rootdir=$(findmnt --list | awk -v dev=$dev '$2 == dev && $1 != "/" {print $1}')
-	echo ${rootdir}/${rpath} $'\t' $dst
+	echo ${rootdir}/${rpath} $'\t' $dst $'\t' $others
 done < <(findmnt --list | awk '$2 ~ /\]$/')
