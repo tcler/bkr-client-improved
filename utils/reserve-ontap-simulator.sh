3d066183978ade4cdcaccd8a3ab84af92fe6d39c
Jianhong Yin <jiyin@redhat.com>
2020-08-24 10:02:33 +0800

    utils/reserve-ontap-simulator.sh: add support for single node
    Signed-off-by: Jianhong Yin <yin-jianhong@163.com>

--- utils/reserve-ontap-simulator.sh
+++ utils/reserve-ontap-simulator.sh
@@ -19,7 +19,9 @@ while true; do
 done
 
 HostDistro=Fedora-32
-submitlog=$(runtest --random ${HostDistro:-Fedora-32} '--cmd=git clone https://github.com/tcler/kiss-vm-ns; sudo make -C kiss-vm-ns; sudo vm --prepare; wget ftp://fs-qe.usersys.redhat.com/pub/Netapp-Simulator/vsim-netapp-DOT9.7-cm_nodar.ova; tar vxf vsim-netapp-DOT9.7-cm_nodar.ova; for i in {1..4}; do qemu-img convert -f vmdk -O qcow2 vsim-NetAppDOT-simulate-disk${i}.vmdk vsim-NetAppDOT-simulate-disk${i}.qcow2; done; git clone https://github.com/tcler/ontap-simulator-in-kvm; bash ontap-simulator-in-kvm/ontap-simulator-9.7-two-node.sh' -hr=memory\>=16384 --hr=kv-DISKSPACE\>=500000)
+script=ontap-simulator-9.7-two-node.sh
+[[ -n "$1" ]] && script=ontap-simulator-9.7-single-node.sh
+submitlog=$(runtest --random ${HostDistro:-Fedora-32} '--cmd=git clone https://github.com/tcler/kiss-vm-ns; sudo make -C kiss-vm-ns; sudo vm --prepare; wget ftp://fs-qe.usersys.redhat.com/pub/Netapp-Simulator/vsim-netapp-DOT9.7-cm_nodar.ova; tar vxf vsim-netapp-DOT9.7-cm_nodar.ova; for i in {1..4}; do qemu-img convert -f vmdk -O qcow2 vsim-NetAppDOT-simulate-disk${i}.vmdk vsim-NetAppDOT-simulate-disk${i}.qcow2; done; git clone https://github.com/tcler/ontap-simulator-in-kvm; bash ontap-simulator-in-kvm/'$script -hr=memory\>=16384 --hr=kv-DISKSPACE\>=500000)
 
 echo "$submitlog"
 job=$(egrep -o J:[0-9]+ <<<"$submitlog")
@@ -49,7 +51,10 @@ echo -e "\nInfo: waiting host OS install done ..."
 while ! nc $host 22 </dev/null &>/dev/null; do sleep 10; echo -n .; done
 
 echo -e "\nInfo: waiting ontap simulator install done ..."
-while ! grep /distribution/command.*status=.Completed < <(bkr job-results $job --prettyxml); do sleep 10; echo -n .; done
+while ! grep -q /distribution/command.*status=.Completed < <(bkr job-results $job --prettyxml); do sleep 10; echo -n .; done
+grep /distribution/command.*status=.Completed < <(bkr job-results $job --prettyxml)
+
+echo -e "\nInfo: getting data lif's info ..."
 task=$(bkr job-results $job --prettyxml | sed -rn '/name=.\/distribution\/command/{s;^.*id="([0-9]+)".*$;\1;; p}')
 logurl=$(bkr job-logs $job|grep /$task/.*taskout.log)
-curl -s -L $logurl|egrep '^[[:space:]]*lif[12].[01]|uniq'
+curl -s -L $logurl|egrep '^[[:space:]]*lif[12]|uniq'
