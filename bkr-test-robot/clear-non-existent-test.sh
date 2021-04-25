cc79ce7cf4b9ccaf1482b46670445919d4ca015b
Jianhong Yin <jiyin@redhat.com>
2021-04-25 12:47:41 +0800

    bkr-test-robot/clear-non-existent-test.sh: auto detect possible prefix
    Signed-off-by: Jianhong Yin <yin-jianhong@163.com>

--- bkr-test-robot/clear-non-existent-test.sh
+++ bkr-test-robot/clear-non-existent-test.sh
@@ -2,7 +2,10 @@
 
 repodir=${1%/}
 prefix=${2}
-[[ -z "$prefix" ]] && prefix=/${repodir##*/}/
+[[ -z "$prefix" ]] && {
+	auto_prefix=yes
+	prefix=/${repodir##*/}/
+}
 
 [[ $# = 0 ]] && {
 	cat <<-EOF
@@ -11,14 +14,24 @@ prefix=${2}
 
 	Example:
 	  $0 ~/ws/testcase/kernel
-	  $0 ~/ws/testcase/nfs-utils.git /nfs-utils/
-	  $0 ~/ws/testcase/libtirpc /CoreOS/libtirpc/
+	  $0 ~/ws/testcase/nfs-utils.git [/nfs-utils/]  #optional
+	  $0 ~/ws/testcase/libtirpc [/CoreOS/libtirpc/] #optional
+	  $0 ~/ws/testcase/kernel.orig  /kernel.orig/
 	EOF
 	exit 0
 }
 
 ## find non-existent test cases
 while read _case; do
+	if [[ "$auto_prefix" = yes ]]; then
+		case ${_case} in
+		${prefix}*) :;;
+		${prefix%.git}*) prefix=${prefix%.git};;
+		/CoreOS${prefix}*) prefix=/CoreOS${prefix};;
+		/CoreOS${prefix%.git}*) prefix=/CoreOS${prefix%.git};;
+		esac
+	fi
+
 	if [[ "$_case" != ${prefix}* ]]; then
 		continue
 	fi
@@ -32,7 +45,7 @@ done < <(bkr-autorun-stat -t | awk -F: '{print $1}' | uniq)
 
 ## remove them from data base
 cnt=${#testcaseList[@]}
-max=64
+max=32
 if [[ $cnt -gt 0 ]]; then
 	if [[ $cnt -gt $max ]]; then
 		echo "[warn] the count of non-existent test cases is more than $max"
