0e77dc4185b9ee30f905029eae8b0d348dc4a620
Jianhong Yin <jiyin@redhat.com>
2022-04-07 09:09:46 +0800

    utils/port-available.sh: optimize code
    Signed-off-by: Jianhong Yin <yin-jianhong@163.com>

--- utils/port-available.sh
+++ utils/port-available.sh
@@ -1,48 +1,37 @@
 #!/bin/bash
 
-which nc &>/dev/null || dep=nmap-ncat
-[[ -n "$dep" ]] && {
-	echo -e "{Info} install dependences ..."
-	sudo yum install -y $dep >&2
-}
-
-Usage() {
-	echo "Usage: $0 [-h] [-d] [-w|-wait] <host:port>"
-}
-
-port_available() {
-	#nc $1 $2 </dev/null &>/dev/null
-	nc $(grep -q -- '-z\>' < <(nc -h 2>&1) && echo -z) $1 $2 </dev/null &>/dev/null
-}
-
-# parse options
-AT=()
+_at=()
 for arg; do
 	case "$arg" in
-	-h)       Usage; exit;;
-	-d)       DEBUG=yes;;
-	-w|-wait) WAIT=yes;;
-	-*)       echo "{WARN} unkown option '${arg}'" >&2;;
-	*)        AT+=($arg);;
+	(--w*|-w*) WAIT=yes; [[ "$arg" = *=* ]] && TIME=${arg/*=/};;
+	(*) _at+=($arg);;
 	esac
+	shift
 done
 
-[[ -z "${AT}" ]] && {
-	Usage >&2
-	exit 1
+port_available() {
+	if grep -q -- '-z\>' < <(nc -h 2>&1); then
+		nc -z $1 $2 </dev/null &>/dev/null
+	elif command -v nmap >/dev/null; then
+		nmap $1 -p $2 | grep -q open
+	else
+		timeout 0.1 curl -s -v telnet://$1:$2 |& grep -q ^..Connected
+	fi
 }
 
-if [[ "$AT" = [* ]]; then
-	IFS=']' read addr port <<<"${AT}"
-	addr="${addr#[}"
-	port="${port#:}"
+rc=1
+if [[ "$WAIT" != yes ]]; then
+	port_available "${_at[@]}"; rc=$?
 else
-	IFS=: read addr port <<<"${AT}"
+	TIME=${TIME//[^0-9]/}
+	TIME=${TIME:-forever}
+	CNT=$(((TIME+10)/10))
+	echo "[INFO] waiting port ${_at[@]} available, max time(${TIME})"
+	for ((i=0; i<CNT; i++)); do
+		port_available "${_at[@]}"; rc=$?
+		[[ $rc = 0 ]] && break
+		[[ "$TIME" = forever ]] && i=0
+		sleep 10
+	done
 fi
-
-[[ "$DEBUG" = yes ]] && echo "{info} $addr $port"
-if [[ "$WAIT" = yes ]]; then
-	until port_available $addr $port; do sleep 1; done
-else
-	port_available $addr $port
-fi
+exit $rc
