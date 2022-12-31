a36f6e7961ef903f94037e0f76ff5f419b329262
Jianhong Yin <jiyin@redhat.com>
2022-12-31 22:17:19 +0800

    bkr-runtest/bkr-update.sh: and retry and trap cleanup() code
    Signed-off-by: Jianhong Yin <yin-jianhong@163.com>

--- bkr-runtest/bkr-update.sh
+++ bkr-runtest/bkr-update.sh
@@ -14,12 +14,14 @@ _confdir=/etc/$_repon
 install_bkr_client_improved() {
 	local url=https://github.com/tcler/$_repon
 	local clonedir=$(mktemp -d)
-	git clone $url $clonedir
+	for ((i=0;i<8;i++)); do git clone $url $clonedir && break || sleep 2; done
 	make -C $clonedir
 	rm -rf $clonedir
 }
 
 tmpf=$(mktemp)
+cleanup() { rm -rf $tmp; }
+trap cleanup SIGINT SIGQUIT SIGTERM
 wget -qO- http://api.github.com/repos/tcler/$_repon/commits/master -O $tmpf
 if cmp $tmpf $_confdir/version 2>/dev/null; then
 	echo "[Info] you are using the latest version"
