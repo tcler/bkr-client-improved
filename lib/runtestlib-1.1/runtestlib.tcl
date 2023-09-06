c2de33cf26f9ffd2b4467b5312125995d4a40702
Jianhong Yin <jiyin@redhat.com>
2023-09-06 17:45:08 +0800

    runtest: support distro like 9n to get the latest nightly build
    Signed-off-by: Jianhong Yin <yin-jianhong@163.com>

--- lib/runtestlib-1.1/runtestlib.tcl
+++ lib/runtestlib-1.1/runtestlib.tcl
@@ -203,7 +203,15 @@ proc ::runtestlib::expandDistro {distroStr} {
 	lappend distrol {*}[split $distroStr ", "]
 	set distronl {}
 	foreach d $distrol {
-		if [regexp -- {^(alt-)?[0-9]} $d] { set d [exec bash -c "getLatestRHEL $d; :"] }
+		if [regexp -- {^[0-9]+n$} $d] {
+			set d [string map {n {}} $d]
+			if {$d eq "7"} {
+				set pat {RHEL-7.9-updates-[0-9]{8}\.[0-9]+}
+			} else {
+				set pat [string map "$d RHEL-$d.\[0-9]+.\[0-9]+-\[0-9]{8}\.\[0-9]+" $d]
+			}
+			set d [exec bash -c "distro-compose -dlist|grep -E '$pat'|head -1; :"]
+		} elseif [regexp -- {^(alt-)?[0-9]} $d] { set d [exec bash -c "getLatestRHEL $d; :"] }
 		lappend distronl ${d}
 	}
 	set distroStr [join $distronl ,]
