767eac6dc93b0a7f55f0d910c49817f6e059194a
Jianhong Yin <jiyin@redhat.com>
2023-10-14 01:32:09 +0800

    Update the default ca-trust file list
    Signed-off-by: Jianhong Yin <yin-jianhong@163.com>

--- utils/brewinstall.sh
+++ utils/brewinstall.sh
@@ -265,7 +265,7 @@ fi
 
 # fix ssl certificate verify failed
 # https://certs.corp.redhat.com/ https://docs.google.com/spreadsheets/d/1g0FN13NPnC38GsyWG0aAJ0v8FljNDlqTTa35ap7N46w/edit#gid=0
-(cd /etc/pki/ca-trust/source/anchors && curl -Ls --remote-name-all https://certs.corp.redhat.com/{2022-IT-Root-CA.pem,2015-IT-Root-CA.pem,ipa.crt} && update-ca-trust)
+(cd /etc/pki/ca-trust/source/anchors && curl -Ls --remote-name-all https://certs.corp.redhat.com/{2022-IT-Root-CA.pem,2015-IT-Root-CA.pem,ipa.crt,mtls-ca-validators,RH-IT-Root-CA.crt} && update-ca-trust)
 
 if [[ ${#builds[@]} = 0 ]]; then
 	if [[ "$FLAG" = debugkernel || "$ONLY_DEBUG_INFO" = yes ]]; then
