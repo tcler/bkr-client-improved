0279e08dddcb6a51001c824a8053e576d8422252
Jianhong Yin <jiyin@redhat.com>
2023-10-30 17:51:07 +0800

    add new script: misc/restraint-prbuild.sh
    Signed-off-by: Jianhong Yin <yin-jianhong@163.com>

---
+++ misc/restraint-prbuild.sh
@@ -0,0 +1,161 @@
+#!/bin/bash
+#jiyin@redhat.com
+#be used to create a Restraint build of a specific PR,commit or master
+
+P=$0; [[ $0 = /* ]] && P=${0##*/}
+User=restraint-harness
+PrjName=restraint
+PrjPath=${User}/${PrjName}
+
+_requires_cmds() {
+	local cmd= rc=0
+	for cmd; do
+		command -v $cmd &>/dev/null || {
+			echo -e "\033[1;31m{ERROR} command  \033[1;34m$cmd\033[1;31m  is required by $P, but not found\033[0m" >&2
+			let rc++
+		}
+	done
+	return $rc
+}
+_requires_cmds rhpkg brew || exit 2
+
+## argparse
+Usage() {
+	echo "Usage: $P <prID|commitID|master> [target: beaker-harness-rhel-\$N|eng-fedora-\$N] [--arches=<arch,list|all>] [--prj=tcler/restraint] [-n|--dry]"
+	echo "Args and Opthers:"
+	echo "  \$1            a {PR(pull request) ID} or {commit ID(sha)} or {master}"
+	echo "  \$2            the build target name: beaker-harness-rhel-\$N|eng-fedora-\$N  #default:beaker-harness-rhel-9"
+	echo "  -h,--help     output this help info"
+	echo "  -a,--arches:  comma seperated arch list: aarch64,ppc64le  #default is x86_64, 'all' means all availables"
+	echo "  -p,--prj:     build from fork: anotherUser/newReponame"
+	echo "  -n,--dry:     only download tarball and create srpm but don't realy submmit the build"
+	echo -e "\nExamples:"
+	echo "  $P 303"
+	echo "  $P 303 beaker-harness-rhel-7"
+	echo "  $P 73ad3be eng-fedora-40 -a ppc64le,s390x"
+	echo "  $P master -p tcler/restraint -a all"
+}
+_at=`getopt -o ha:p:n \
+	--long help \
+	--long arch: --long arches: \
+	--long prj: \
+	--long dry \
+    -a -n '$P' -- "$@"`
+eval set -- "$_at"
+
+while true; do
+	case "$1" in
+	-h|--help)      Usage; shift 1; exit 0;;
+	-a|--arch*)     arches="$2"; shift 2;;
+	-p|--prj)       prjPath="$2"; fork=${prjPath%%/*}; shift 2;;
+	-n|--dry)       dryRun="yes"; shift 1;;
+	--) shift; break;;
+	esac
+done
+
+[[ $# = 0 ]] && {
+	Usage >&2
+	exit 1
+}
+
+prcoID=$1
+prjPath=${prjPath:-$PrjPath}
+bhbranch=${2:-beaker-harness-rhel-9}
+webBaseUrl=https://github.com/${prjPath}
+apiBaseUrl=https://api.github.com/repos/${prjPath}
+
+pullBaseUrl=${apiBaseUrl}/tarball/pull
+commitBaseUrl=${webBaseUrl}/archive
+masterUrl=${webBaseUrl}/archive/refs/heads/master.tar.gz
+
+if [[ "${prcoID}" =~ ^[1-9][0-9]+$ ]]; then
+	_type=pr; _infix=pr.${prcoID}
+	tarballUrl=${pullBaseUrl}/${prcoID}/head
+elif [[ "${prcoID,,}" =~ ^[0-9a-f]+$ ]]; then
+	_type=co; _infix=co.${prcoID}
+	tarballUrl=${commitBaseUrl}/${prcoID}.tar.gz
+elif [[ "${prcoID,,}" = ma* ]]; then
+	_coID=$(curl -Ls ${apiBaseUrl}/commits/master | awk -F'[ :",]+' '$2=="sha"{print substr($3,0,7); exit}')
+	_type=ma; _infix=master.$_coID
+	tarballUrl=$masterUrl
+else
+	{ Usage >&2; exit 1; }
+fi
+[[ -n "$fork" ]] && _infix=${fork}.${_infix}
+
+_spec_repodir=~/restraint-build
+[[ -d "$_spec_repodir" ]] || rhpkg clone restraint $_spec_repodir
+spec_repodir=~/Restraint-prbuild-${_infix}
+[[ -d "$spec_repodir" ]] || cp -r $_spec_repodir $spec_repodir
+(cd $spec_repodir || exit 1 && git checkout .
+git checkout $bhbranch && git pull --rebase
+tarballName=${PrjName}-${_infix/./-}.tgz
+
+#download PR tarball
+echo -e "\033[1;34m{INFO} download tarball of ${prcoID} from $tarballUrl\033[0m"
+rm -f ${tarballName}
+curl -L -f $tarballUrl -o ${tarballName}
+if [[ "$?" != 0 ]]; then
+	echo -e "\033[1;31m{ERROR}: downloading tarball fail, please verify the pr/commit ID or network\033[0m" >&2;
+	exit 1;
+fi
+
+#get topdir-name(format: restraint-harness-restraint-$commitID) from tarball
+#and get restraint.spec from it
+oldTopdir=$(tar taf $tarballName | sed -rn "1{s|/$||; p; q}")
+tar -C . -zxf ${tarballName} --warning=no-timestamp
+cp $oldTopdir/restraint.spec .
+
+#get version info from restraint.spec
+#and gen new topdir-name that must be restraint-${ver}
+ver=$(awk '/Version:/{print $2}' restraint.spec)
+newTopdir=restraint-${ver}
+
+#gen new tarballName that must not be restraint-${ver}.tar.gz
+#Otherwise rhpkg will always download and overwrite it when creating srpm
+ntarballName=${newTopdir}-${_infix/./-}.tar.gz
+
+#extract tarball, rename topdir and re-archive
+echo -e "\033[1;34m{INFO} generate the tarball(${ntarballName}) that will be included in srpm\033[0m"
+rm -rf $newTopdir && mv -T $oldTopdir $newTopdir
+tar zcf ${ntarballName} ${newTopdir}
+
+#gen relInfix and update Release value in spec file
+relInfix=${_infix}
+[[ $_type = pr ]] && relInfix=${oldTopdir/*-/${_infix}.}
+sed -ri -e "/Release:/{s/%/.${relInfix:-wrongInfix.}&/}" \
+	-e "/Source0:/{s|/[^/]+$|/${ntarballName}|}" restraint.spec
+
+#gen srpm and submit scratch-build
+rm -f *.rpm
+echo -e "\033[1;34m{INFO} generate the srpm file\033[0m"
+rhpkg srpm
+srpmfile=$(ls *.src.rpm)
+if [[ -z "$srpmfile" ]]; then
+	echo "{Error} create srpm file fail" >&2
+	exit 2
+fi
+
+arches=${arches:-x86_64}
+##tips: like curl rhpkg doesn't support '--options=xyz', please use '--options xyz'
+[[ "${arches,,}" != all ]] && archOpt="--arches ${arches//,/ }"
+echo -e "\033[1;34m{DEBUG} run: rhpkg scratch-build --srpm $srpmfile $archOpt\033[0m";
+[[ "$dryRun" = yes ]] && exit 0
+rhpkg scratch-build --srpm $srpmfile $archOpt |& tee build-screen.log
+
+#get build state
+prBuildInfoFile=/tmp/rstrnt-prbuild-${_infix}.info
+brewTaskID=$(awk '/Created task:/{print $NF}' build-screen.log)
+brew taskinfo -r $brewTaskID |& tee $prBuildInfoFile
+buildStat=$(awk '/^State:/{print $NF}' $prBuildInfoFile)
+if [[ "$buildStat" != closed ]]; then
+	cat <<-LOG >&2
+	{Error} restraint PR($prcoID) build failed. see:
+	  https://brewweb.engineering.redhat.com/brew/taskinfo?taskID=$brewTaskID
+	LOG
+	exit 1
+else
+	#fixme: if want create a permenant yum repo for test section
+	echo -e "\033[1;34m{INFO} restraint PR($prcoID) build success, see you in test section\033[0m"
+fi
+)
