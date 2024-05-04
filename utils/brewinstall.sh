326d697ccdc09fe47cbf9b78bf291f0531fc475f
Jianhong Yin <jiyin@redhat.com>
2024-05-04 19:17:13 +0800

    utils/brewinstall.sh: fix rpmFilter userspace pkg checking code 2
    Signed-off-by: Jianhong Yin <yin-jianhong@163.com>

--- utils/brewinstall.sh
+++ utils/brewinstall.sh
@@ -197,7 +197,7 @@ rpmFilter() {
 		#echo "[rpmFilter] _check: $url" >&2
 		file=${url##*/}
 		# we didn't consider to filter any userspace packages
-		if [[ "${url}" != */kernel/* ]]; then
+		if [[ "${url}" = *://* && "${url}" != */kernel/* ]] || [[ "${url}" = "${file}" && "${file}" != kernel* ]]; then
 			echo "${file} didn't belong ^kernel* package, will install!" >&2
 			echo -e "\E[01;36m userspace accept: $url\E[0m" >&2
 			echo "$url"
