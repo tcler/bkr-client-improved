12b0629811e410a9b2fa91c3ac1beace2b05a7bd
Jianhong Yin <jiyin@redhat.com>
2024-08-02 12:06:23 +0800

    utils/mr-build-repos.sh: check and add rtk flag for repo
    Signed-off-by: Jianhong Yin <yin-jianhong@163.com>

--- utils/mr-build-repos.sh
+++ utils/mr-build-repos.sh
@@ -12,16 +12,20 @@ preTestIssues="$*"
 }
 
 for issue in ${preTestIssues}; do
-	echo -e $'\E[0;33;44m'"{get-mr-builds-by} jira-issue.py '${issue}' 'Testable Builds'" $'\E[0m'>&2
+	echo -e $'\E[0;33;44m'"{get-mr-builds-by} jira-issue.py '${issue}' 'Testable Builds' fixVersions components" $'\E[0m'>&2
 	issueInfo=$(jira-issue.py "${issue}" 'Testable Builds' fixVersions components)
+	title=$(sed -n '/Title: */{s///;p;q}' <<<"$issueInfo")
 	repos=$(echo "${issueInfo}" | awk '/Repo.URL:/{print $NF}')
 	if [[ -n "${repos}" ]]; then
 		rhelVersion=$(echo "${issueInfo}" | sed -rn "/BEGIN fixVersions/,/END fixVersions/{/^.*name='([^']+)'.*$/{s//\\1/;p}}")
-		component=$(echo "${issueInfo}" | sed -rn "/BEGIN components/,/END components/{/^.*name='([^']+)'.*$/{s//\\1/;p}}")
 		rhelVersion=${rhelVersion#*-}
-		echo -e "${issue}  ${rhelVersion}  #${component// /}"
+		component=$(echo "${issueInfo}" | sed -rn "/BEGIN components/,/END components/{/^.*name='([^']+)'.*$/{s//\\1/;p}}")
+		component=${component// /}; [[ "$component" = *kernel-rt* ]] && component=$'\E[0;33;45m'"$component"$'\E[0m'
+		echo -e "${issue}  ${rhelVersion}  #${component}  [$title]"
 		for repo in $repos; do
-			dbgkOpt=; [[ $repo = *_debug* ]] && dbgkOpt=' -debugk'
+			dbgkOpt=
+			[[ $repo = *_debug* ]] && dbgkOpt+=' -debugk'
+			[[ $repo = *x86_64* ]] && yum-repo-query.sh $repo |& grep -q kernel-rt && dbgkOpt+=' rtk'
 			echo -e "\t$repo$dbgkOpt"
 		done
 	else
